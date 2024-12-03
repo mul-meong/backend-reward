4051627759ffe33a5778da0edc97f1870689896f
SeongGwang <seonggwangdev@gmail.com>
2024-12-03 12:26:28 +0900

    ✨ feat: 피드,쇼츠,콘테스트 글 작성시 이벤트 Consume 기능 구현

--- src/main/java/com/mulmeong/reward/point/infrastructure/PointKafkaConsumer.java
+++ src/main/java/com/mulmeong/reward/point/infrastructure/PointKafkaConsumer.java
@@ -4,11 +4,15 @@ import com.mulmeong.event.comment.FeedCommentCreateEvent;
 import com.mulmeong.event.comment.FeedRecommentCreateEvent;
 import com.mulmeong.event.comment.ShortsCommentCreateEvent;
 import com.mulmeong.event.comment.ShortsRecommentCreateEvent;
+import com.mulmeong.event.contest.ContestPostCreateEvent;
+import com.mulmeong.event.feed.FeedCreateEvent;
 import com.mulmeong.event.member.MemberCreateEvent;
+import com.mulmeong.event.shorts.ShortsCreateEvent;
 import com.mulmeong.reward.point.application.MemberPointEventService;
 import com.mulmeong.reward.point.domain.entity.EventType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -21,11 +25,14 @@ public class PointKafkaConsumer {
 
     private final MemberPointEventService memberPointEventService;
 
+    @Value("${event.feed.pub.topics.feed-create.name}")
+    private String testTopic;
+
     @KafkaListener(topics = "${event.member.pub.topics.member-create.name}",
             containerFactory = "memberCreateEventListener")
     public void handleMemberCreatedEvent(MemberCreateEvent event) {
-        log.info("Consumed 회원 생성 이벤트 : {}", event);
         memberPointEventService.createMemberPointDocument(event);
+        log.info("Consumed 회원 생성 이벤트 : {}", event);
     }
 
     /* 댓글 작성시 포인트 증가(피드|숏츠 * 댓글|대댓글 총 4종류 */
@@ -33,6 +40,7 @@ public class PointKafkaConsumer {
             containerFactory = "feedCommentCreateEventListener")
     public void handleFeedCommentCreatedEvent(FeedCommentCreateEvent event) {
         memberPointEventService.addPointByEvent(event.getMemberUuid(), COMMENT_CREATED);
+        log.info("Consumed 피드 댓글 생성 이벤트 : {}", event);
     }
 
     @KafkaListener(topics = "${event.comment.pub.topics.feed-recomment-create.name}",
@@ -56,5 +64,24 @@ public class PointKafkaConsumer {
         log.info("Consumed 숏츠 대댓글 생성 이벤트 : {}", event);
     }
 
-    /* */
+    @KafkaListener(topics = "${event.feed.pub.topics.feed-create.name}",
+            containerFactory = "feedCreateEventListener")
+    public void handleFeedCreatedEvent(FeedCreateEvent event) {
+        memberPointEventService.addPointByEvent(event.getMemberUuid(), EventType.FEED_CREATED);
+        log.info("Consumed 피드 생성 이벤트 : {}", event);
+    }
+
+    @KafkaListener(topics = "${event.shorts.pub.topics.shorts-create.name}",
+            containerFactory = "shortsCreateEventListener")
+    public void handleShortsCreatedEvent(ShortsCreateEvent event) {
+        memberPointEventService.addPointByEvent(event.getMemberUuid(), EventType.SHORTS_CREATED);
+        log.info("Consumed 숏츠 생성 이벤트 : {}", event);
+    }
+
+    @KafkaListener(topics = "${event.contest.pub.topics.contest-post-create.name}",
+            containerFactory = "contestPostCreateEventListener")
+    public void handleContestPostCreatedEvent(ContestPostCreateEvent event) {
+        memberPointEventService.addPointByEvent(event.getMemberUuid(), EventType.CONTEST_JOINED);
+        log.info("Consumed 콘테스트 포스트 생성 이벤트 : {}", event);
+    }
 }
